bc4403f31b95ff9f337fc04262c736d047d37c0b
Bunyoddff <bkhusanov444@gmail.com>
2022-12-29 20:19:16 +0500

    py005

--- if05.py
+++ if05.py
@@ -6,6 +6,11 @@ def main(n):
     Returns:
         int: return answer.
     """
-    s=max(str(n))
+    d1=n//10000
+    d2=(n//1000)%10
+    d3=(n//100)%10
+    d4=((n//10)%100)%10
+    d5=n%10
+    s=max(d1,d2,d3,d4,d5)
     return s
 print(main(23546))
